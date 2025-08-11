a2564ee29613965b00f0b56fa35de74f7192e8b0
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2025-08-11 22:49:51 +0100

    Add code

---
+++ employeemgtsystem/src/main/java/com/javarchitect/employeemgtsystem/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.javarchitect.employeemgtsystem.service;
+
+import com.javarchitect.employeemgtsystem.entity.Employee;
+import java.util.List;
+
+public interface EmployeeService {
+
+	List<Employee> findAll();
+	
+	Employee findById(int theId);
+	
+	void save(Employee theEmployee);
+	
+	void deleteById(int theId);
+}
